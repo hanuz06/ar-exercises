de54f0487fc3a4dd52151c940e32f8e38a99facb
hanuz06 <lee1975uz@gmail.com>
2019-12-10 06:40:56 +0000

    add after_create callback

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,13 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, presence: true  
   validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200  }
   validates :store_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
+  after_create  :random_password
+
+  private 
+
+  def random_password 
+    @password = ('0'..'z').to_a.shuffle.first(8).join
+    Employee.update(id, password: @password)
+  end
 end
